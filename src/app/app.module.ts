b5dac3a99ab099b4658d61e52b93be285f83f6ff
Juan Vicente Torres <j.v.torres.fernandez@gmail.com>
2019-06-09 15:49:04 +0200

    Create the event list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,16 @@ import { MatListModule } from '@angular/material/list';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { EventListComponent } from './event-list/event-list.component';
 /* Libraries */
 import 'hammerjs';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/event.ts
@@ -0,0 +1,11 @@
+/**
+ * Class which defines an event
+ */
+export class Event {
+    id: string; // event identifier
+    title: string; // event title
+    location: string; // event location
+    date: Date; // event date
+    description: string; // event description
+    addedBy: string; // event user
+  }
