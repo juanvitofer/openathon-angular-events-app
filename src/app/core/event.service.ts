014237e4d87c986f709bc8ef04c06bfae05faf75
Juan Vicente Torres <j.v.torres.fernandez@gmail.com>
2019-06-13 21:39:45 +0200

    Transform data from HttpClient

--- src/app/core/event.service.ts
+++ src/app/core/event.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 /* Import HttpClient for making the request over the HTTP protocol */
 /* Import HttpHeaders to add simple headers */
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 /* Import RxJS library */
-import { Observable } from 'rxjs';
+import { Observable, throwError  } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+/** Import enviroment file */
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +27,29 @@ export class EventService {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
-    // Set the .json file route and the header
-    return this.http.get('assets/events.json', { headers });
+    // Set the enviroment and header
+    return this.http.get(environment.apiURL + 'events', { headers }).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
+
+  /**
+   * Method which handles an error
+   * @param error: error to handle
+   */
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues about what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // Return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
   }
 }
